f8e959da3ca6c9debce99514b4c02619b35f3ef3
Akshay Verma <44718434+averma1010@users.noreply.github.com>
2023-11-15 22:27:08 -0500

    Update Akshay_Test_Script.py

--- Code/Akshay_Test_Script.py
+++ Code/Akshay_Test_Script.py
@@ -2,15 +2,24 @@ import torch
 import torch.nn as nn
 import math
 
+'''
+Hi, A lot of comments here would be just me talking to myself and reminding what the function is for and is doing.
+'''
+
 class InputEmbedding(nn.Module):
 
-    def __init__(self, d_model: int, vocab_size: int):
-        super().__init__()
+    def __init__(self, d_model: int, vocab_size: int):  ## This is called a constructor. Initializes the instance variables or you could say the inputs
+        '''
+        vocab size : as the name suggests it's the unique words in our data
+        d_model : Dimension vector of each word/token
+        '''
+        super().__init__()  ## super() is used to call a method from the parent class ( in this case the parent class is Neural Network for pytorch)
         self.d_model = d_model
         self.vocab_size = vocab_size
         self.embedding  = nn.Embedding(vocab_size, d_model)
 
     def forward(self, x):
+        ## Using nn.Embedding to create an input layer
         return self.embedding(x) * math.sqrt(self.d_model)
 
 class PositionalEmbedding(nn.Module):
@@ -20,3 +29,4 @@ class PositionalEmbedding(nn.Module):
         self.d_model = d_model
         self.seq_lens = seq_lens
         self.dropout = nn.Dropout(dropout)
+
