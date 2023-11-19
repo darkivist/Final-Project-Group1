62cde359ea8f4a062e714766de7eb4ce529a14db
Carrie Magee <mageec@gwu.edu>
2023-11-19 03:55:28 +0000

    updating

--- Code/Carrie_Model.py
+++ Code/Carrie_Model.py
@@ -190,9 +190,9 @@ class MathWordProblemDataset(Dataset):
         )
 
         return {
-            "input_ids": inputs["input_ids"].squeeze(),
-            "attention_mask": inputs["attention_mask"].squeeze(),
-            "labels": labels["input_ids"].squeeze(),
+            "input_ids": inputs["input_ids"].squeeze().to(device),
+            "attention_mask": inputs["attention_mask"].squeeze().to(device),
+            "labels": labels["input_ids"].squeeze().to(device),
         }
 
 # Loading Data
@@ -215,13 +215,12 @@ loss_function = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
 
 # Training loop
-num_epochs = 5
+num_epochs = 6
 for epoch in range(num_epochs):
     for batch in data_loader:
-        input_ids = batch["input_ids"].to(device)
-        attention_mask = batch["attention_mask"].to(device)
-        labels = batch["labels"].to(device)
-
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        labels = batch["labels"]
         outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
         loss = outputs.loss
 
@@ -231,5 +230,34 @@ for epoch in range(num_epochs):
 
         print(f"Epoch: {epoch + 1}, Batch Loss: {loss.item()}")
 
+
+# %%
+# Input text to test
+input_text = "Carrie has 6 pineapples. She ate 3. How many does she have left?"
+
+# Tokenize the input
+input_ids = tokenizer.encode(input_text, return_tensors="pt", max_length=532, truncation=True).to(device)
+
+# Generate output from the model
+output_ids = model.generate(
+    input_ids,
+    max_length=532,
+    num_beams=10,
+    no_repeat_ngram_size=2,
+    top_k=20,
+    top_p=0.95,
+    temperature=0.7,
+    pad_token_id=tokenizer.eos_token_id,  # Set pad token ID
+    attention_mask=torch.ones(input_ids.shape, device=device),  # Set attention mask
+)
+
+# Decode the generated output
+generated_equation = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+
+# Print the results
+print("Input Math Word Problem:")
+print(input_text)
+print("\nGenerated Equation:")
+print(generated_equation)
 
 # %%
--- Code/T5_transformer.py
+++ Code/T5_transformer.py
@@ -84,3 +84,107 @@ with torch.no_grad():  # No need for gradient computation during inference
 
 print(f"Test Sentence: {test_sentence}")
 print(f"Generated Prediction: {prediction}")
+#%%
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer, T5ForConditionalGeneration
+
+class MathWordProblemDataset(Dataset):
+    def __init__(self, dataframe, tokenizer, max_sequence_length):
+        self.mwp = dataframe["Question"].tolist()
+        self.equation = dataframe["Equation"].tolist()
+        self.tokenizer = tokenizer
+        self.max_sequence_length = max_sequence_length
+
+    def __len__(self):
+        return len(self.mwp)
+
+    def __getitem__(self, idx):
+        mwp_text = self.mwp[idx]
+        equation_text = self.equation[idx]
+
+        inputs = self.tokenizer(
+            f"translate English to Math: {mwp_text}",
+            padding="max_length",
+            truncation=True,
+            max_length=self.max_sequence_length,
+            return_tensors="pt",
+        )
+
+        labels = self.tokenizer(
+            equation_text,
+            padding="max_length",
+            truncation=True,
+            max_length=self.max_sequence_length,
+            return_tensors="pt",
+        )
+
+        return {
+            "input_ids": inputs["input_ids"].squeeze().to(device),
+            "attention_mask": inputs["attention_mask"].squeeze().to(device),
+            "labels": labels["input_ids"].squeeze().to(device),
+        }
+
+# Loading Data
+df = pd.read_csv('/home/ubuntu/NLP_Main/Final-Project-Group1/Code/cleaned_dataset.csv')
+
+checkpoint = "t5-small"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+tokenizer.pad_token = tokenizer.eos_token
+
+max_sequence_length = 532  # Adjust this based on your data and model requirements
+
+math_word_problem_dataset = MathWordProblemDataset(df, tokenizer, max_sequence_length)
+data_loader = DataLoader(math_word_problem_dataset, batch_size=2, shuffle=True)
+
+# Model
+model = T5ForConditionalGeneration.from_pretrained(checkpoint).to(device)
+
+# Training
+loss_function = torch.nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
+num_epochs = 6
+accumulation_steps = 4  # Accumulate gradients over 4 steps
+for epoch in range(num_epochs):
+    total_loss = 0
+    for i, batch in enumerate(data_loader):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        labels = batch["labels"]
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs.loss
+
+        # Accumulate gradients
+        loss = loss / accumulation_steps
+        loss.backward()
+
+        # Update parameters every accumulation_steps
+        if (i + 1) % accumulation_steps == 0 or i == len(data_loader) - 1:
+            optimizer.step()
+            optimizer.zero_grad()
+
+        total_loss += loss.item()
+
+    print(f"Epoch: {epoch + 1}, Average Loss: {total_loss / len(data_loader)}")
+
+
+#%%
+# Training loop
+num_epochs = 6
+for epoch in range(num_epochs):
+    for batch in data_loader:
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        labels = batch["labels"]
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs.loss
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print(f"Epoch: {epoch + 1}, Batch Loss: {loss.item()}")
+
+# %%
