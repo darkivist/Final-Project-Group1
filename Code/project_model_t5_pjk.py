0223553b1fe12ecfdb6ab2b8dddbe02d6c1353b4
darkivist <kpaulj@protonmail.com>
2023-11-21 09:33:16 -0500

    T5 now generating results.

--- Code/project_model_t5_pjk.py
+++ Code/project_model_t5_pjk.py
@@ -4,27 +4,33 @@ from sklearn.model_selection import train_test_split
 import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer, Seq2SeqTrainer, Seq2SeqTrainingArguments
 
-#load and preprocess data
+#load data
 data = pd.read_csv('train.csv')
 
-#function to replace 'numberx' placeholders with '[NUM]' token
-def replace_number_placeholders(text):
-    return re.sub(r'number\d+', '[NUM]', text)
+#function to replace number0 (etc) values with actual numbers
+def replace_number_placeholders(row):
+    def replace_number(match):
+        number_index = int(match.group(1))  #extract number index
+        return str(row['Numbers'].split(' ')[number_index])  # Replace placeholder with actual value
+
+    return re.sub(r'number(\d+)', replace_number, row['Question'])  # Assuming the column name is 'Question'
 
 #ensure 'ques' is a string
-data['Ques'] = data['Ques'].astype(str)
+data['Question'] = data['Question'].astype(str)
 
 #replace number placeholders
-data['Processed_Ques'] = data['Ques'].apply(replace_number_placeholders)
+data['Processed_Question'] = data.apply(replace_number_placeholders, axis=1)
 
 #train/val split
 train_data, val_data = train_test_split(data, test_size=0.2, random_state=42)
 
-train_questions = train_data['Processed_Ques'].tolist()
+train_questions = train_data['Processed_Question'].tolist()
 train_answers = train_data['Answer'].apply(lambda x: str(x)).tolist()
 
-val_questions = val_data['Processed_Ques'].tolist()
+val_questions = val_data['Processed_Question'].tolist()
 val_answers = val_data['Answer'].apply(lambda x: str(x)).tolist()
+
+print(val_data)
 
 #initializing tokenizer
 tokenizer = T5Tokenizer.from_pretrained('t5-small')
@@ -36,27 +42,32 @@ tokenized_train_outputs = tokenizer(train_answers, return_tensors='pt', padding=
 tokenized_val_inputs = tokenizer(val_questions, return_tensors='pt', padding=True, truncation=True)
 tokenized_val_outputs = tokenizer(val_answers, return_tensors='pt', padding=True, truncation=True)
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 train_dataset = [
     {
-        'input_ids': tokenized_train_inputs['input_ids'][i],
-        'attention_mask': tokenized_train_inputs['attention_mask'][i],
-        'decoder_input_ids': tokenized_train_outputs['input_ids'][i],
-        'decoder_attention_mask': tokenized_train_outputs['attention_mask'][i],
-        'labels': tokenized_train_outputs['input_ids'][i].clone()  # Labels
+        'input_ids': tokenized_train_inputs['input_ids'][i].to(device),
+        'attention_mask': tokenized_train_inputs['attention_mask'][i].to(device),
+        'decoder_input_ids': tokenized_train_outputs['input_ids'][i].to(device),
+        'decoder_attention_mask': tokenized_train_outputs['attention_mask'][i].to(device),
+        'labels': tokenized_train_outputs['input_ids'][i].to(device).clone()  # Labels
     }
     for i in range(len(tokenized_train_inputs['input_ids']))
 ]
 
 val_dataset = [
     {
-        'input_ids': tokenized_val_inputs['input_ids'][i],
-        'attention_mask': tokenized_val_inputs['attention_mask'][i],
-        'decoder_input_ids': tokenized_val_outputs['input_ids'][i],
-        'decoder_attention_mask': tokenized_val_outputs['attention_mask'][i],
-        'labels': tokenized_val_outputs['input_ids'][i].clone()  # Labels
+        'input_ids': tokenized_val_inputs['input_ids'][i].to(device),
+        'attention_mask': tokenized_val_inputs['attention_mask'][i].to(device),
+        'decoder_input_ids': tokenized_val_outputs['input_ids'][i].to(device),
+        'decoder_attention_mask': tokenized_val_outputs['attention_mask'][i].to(device),
+        'labels': tokenized_val_outputs['input_ids'][i].to(device).clone()  # Labels
     }
     for i in range(len(tokenized_val_inputs['input_ids']))
 ]
+
+#print(val_dataset)
+
 
 #define training arguments
 training_args = Seq2SeqTrainingArguments(
@@ -93,6 +104,17 @@ trainer.train()
 train_metrics = trainer.evaluate()
 print(f"Training metrics: {train_metrics}")
 
+
+#show first five records/predictions from val dataset
+for i in range(5):
+    dict_item = val_dataset[i]
+    input_ids = dict_item['input_ids']
+    # Reshape input_ids to add batch and sequence length dimensions
+    input_ids = input_ids.unsqueeze(0).to(device)  # Add batch dimension and move to device
+    outputs = model.generate(input_ids)
+    prediction = tokenizer.decode(outputs[0], skip_special_tokens=True)  # Decode the output without special tokens
+    print(f"Prediction: {prediction}")
+
 def preprocess_word_problem(problem_text):
     #tokenize the problem text
     tokenized_problem = tokenizer(problem_text, return_tensors='pt', padding=True, truncation=True)
@@ -107,7 +129,7 @@ print("Word problem:", word_problem)
 device = 'cuda:0'
 tokenized_input = preprocess_word_problem(word_problem)
 tokenized_input = {key: tensor.to(device) for key, tensor in tokenized_input.items()}
-print("tokenized input:",tokenized_input)
+#print("tokenized input:",tokenized_input)
 
 #wrap in batch
 #batched_input = {
@@ -120,7 +142,7 @@ output = model.generate(input_ids=tokenized_input['input_ids'],
                         attention_mask=tokenized_input['attention_mask'],
                         max_length=200)
 
-print("output:", output)
+#print("output:", output)
 
 #decode the generated output tokens to text
 decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
