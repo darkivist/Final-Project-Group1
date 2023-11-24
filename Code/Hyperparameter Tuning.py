c531510789084724c33a18eee4e7f8f6a3ce58f3
Akshay Verma <44718434+averma1010@users.noreply.github.com>
2023-11-24 14:26:00 -0500

    Hyperparameter tuning
    Not sure if it works or not yet.

---
+++ Code/Hyperparameter Tuning.py
@@ -0,0 +1,23 @@
+import optuna
+from train import train_model
+from config import  get_config
+
+def objective(trial):
+    config = get_config()
+    # Update the config with hyperparameters sampled by Optuna
+    config['batch_size'] = trial.suggest_int('batch_size', 4, 16)
+    config['lr'] = trial.suggest_float('lr', 1e-5, 1e-1, log=True)
+    config['d_model'] = trial.suggest_int('d_model', 256, 1024)
+    config['num_epochs'] = trial.suggest_int('num_epochs', 10, 100)
+
+    # Calling model
+    validation_loss = train_model(config)
+
+
+    return validation_loss  
+
+if __name__ == "__main__":
+    study = optuna.create_study(direction='minimize')
+    study.optimize(objective, n_trials=100)
+    best_params = study.best_params
+    print("Best Hyperparameters:", best_params)
--- Code/Transformer_from_scratch.py
+++ Code/Transformer_from_scratch.py
@@ -45,7 +45,8 @@ class PositionalEncoding(nn.Module):
 
         position = torch.arange(0, seq_lens, dtype=torch.float).unsqueeze(1)
         position = torch.arange(0, seq_lens, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0,d_model,2).float() * (-math.log(10000.0)/d_model))
+        #div_term = torch.exp(torch.arange(0,d_model,2).float() * (-math.log(10000.0)/d_model))
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / (d_model - 1)))
 
         pe[:, 0::2] = torch.sin(position*div_term)
         pe[:, 1::2] = torch.cos(position*div_term)
