0710476b552753e29729aceb62f747c124df49c7
darkivist <kpaulj@protonmail.com>
2023-11-25 13:56:51 -0500

    Updated with hyperparameter tuning function

--- Code/t5_attempt4_pjk.py
+++ Code/t5_attempt4_pjk.py
@@ -7,6 +7,7 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import re
 from sklearn.model_selection import train_test_split
+import optuna
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -88,34 +89,92 @@ val_answers = val_data['Answer'].tolist()
 tokenizer = T5Tokenizer.from_pretrained("t5-small")
 model = T5ForConditionalGeneration.from_pretrained("t5-small")
 
+#define the objective function for optuna
+def objective(trial):
+    learning_rate = trial.suggest_float('learning_rate', 1e-6, 1e-3, log=True)
+    batch_size = trial.suggest_categorical('batch_size', [8, 16, 32, 64])
+    num_training_epochs = trial.suggest_int('num_epochs', 1, 100)
+    model = T5ForConditionalGeneration.from_pretrained("t5-small")
+    tokenizer = T5Tokenizer.from_pretrained("t5-small")
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    #define training arguments
+    training_args = Seq2SeqTrainingArguments(
+        output_dir='./results',
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        logging_dir='./logs',
+        logging_steps=100,
+        save_steps=1000,
+        evaluation_strategy='epoch',
+        eval_steps=500,
+        num_train_epochs=num_training_epochs,
+        predict_with_generate=True
+    )
+
+    #create custom train/val datasets
+    train_dataset = CustomDataset(train_questions, train_equations, train_answers, tokenizer)
+    val_dataset = CustomDataset(val_questions, val_equations, val_answers, tokenizer)
+
+    #instantiate Seq2SeqTrainer
+    trainer = Seq2SeqTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        tokenizer=tokenizer,
+        optimizers=(optimizer, None)
+    )
+
+    #train model
+    trainer.train()
+
+    #get validation loss
+    val_metrics = trainer.evaluate()
+    val_loss = val_metrics['eval_loss']
+
+    return val_loss
+
+#perform hyperparameter tuning
+study = optuna.create_study(direction='minimize')
+study.optimize(objective, n_trials=100)
+
+#get the best hyperparameters
+best_learning_rate = study.best_params['learning_rate']
+best_batch_size = study.best_params['batch_size']
+num_training_epochs = study.best_params['num_epochs']
+print("Best learning rate:", best_learning_rate)
+print("Best batch size:", best_batch_size)
+print("Best number of epochs:", num_training_epochs)
+
 #create custom train/val datasets
 train_dataset = CustomDataset(train_questions, train_equations, train_answers, tokenizer)
 val_dataset = CustomDataset(val_questions, val_equations, val_answers, tokenizer)
 
-#use dataloader to handle batching and shuffling
-train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False)
+#use dataloader to handle batching and shuffling (not required - comment out)
+#train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+#val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False)
 
 #use the dataLoader for training
 model.to(device)
 
-#set optimizer and learning rate
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+#set optimizer and optimized learning rate
+optimizer = torch.optim.Adam(model.parameters(), lr=best_learning_rate)
 
-#define training arguments
+#define optimized training arguments
 training_args = Seq2SeqTrainingArguments(
     output_dir='./results',
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
+    per_device_train_batch_size=best_batch_size,
+    per_device_eval_batch_size=best_batch_size,
     logging_dir='./logs',
     logging_steps=100,
     save_steps=1000,
     evaluation_strategy='epoch',
     eval_steps=500,
-    num_train_epochs=100,
+    num_train_epochs=num_training_epochs,
     predict_with_generate=True
 )
-#instantiate Seq2SeqTrainer
+#instantiate Seq2SeqTrainer again
 trainer = Seq2SeqTrainer(
     model=model,
     args=training_args,
@@ -125,7 +184,7 @@ trainer = Seq2SeqTrainer(
     optimizers=(optimizer, None)
 )
 
-#train model
+#retrain model
 trainer.train()
 
 #display training metrics
@@ -137,18 +196,17 @@ for i in range(5):
     dict_item = val_dataset[i]
     input_ids = dict_item['input_ids']
 
-    # Get validation question and true answer
+    #get validation question and true answer
     val_question = val_dataset.questions[i]  # Accessing the validation question directly from the dataset
     true_answer = val_dataset.answers[i]  # Accessing the true answer directly from the dataset
 
-    # Reshape input_ids to add batch and sequence length dimensions
-    input_ids = input_ids.unsqueeze(0).to(device)  # Add batch dimension and move to device
+    #reshape input_ids to add batch and sequence length dimensions
+    input_ids = input_ids.unsqueeze(0).to(device)
 
-    # Generate prediction
+    #generate prediction
     outputs = model.generate(input_ids)
-    prediction = tokenizer.decode(outputs[0], skip_special_tokens=True)  # Decode the output without special tokens
+    prediction = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    # Print details
     print(f"Validation Question: {val_question}")
     print(f"Predicted Text: {prediction}")
     print(f"True Answer: {true_answer}")
