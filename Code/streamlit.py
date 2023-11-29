12d6d0ba9d6cad42c5c0cc177a77aa2ebedc114f
Ubuntu <ubuntu@ip-10-0-1-180.ec2.internal>
2023-11-29 20:04:41 +0000

    streamlit updates

---
+++ Code/streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# ---------- Titles and headers -----------
+st.title("NLP Group 1: Translating Math Problems")
+st.markdown("""___""")
+st.header("Taking written math problems and using machine translation to compute the answer")
+st.subheader("Paul Kelly, Carrie Magee, Jack McMorrow, Akshay Verma")
+st.divider()
+# -----------------------------------------
+
+
