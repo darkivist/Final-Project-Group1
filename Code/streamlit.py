b4d716eb14846b83e553370e40ae878ed342778b
Ubuntu <jmcmorrow08@gmail.com>
2023-12-08 19:55:44 +0000

    title change

--- Code/streamlit.py
+++ Code/streamlit.py
@@ -21,7 +21,7 @@ model = load_model()
 # ---------- Titles and headers -----------
 st.title("NLP Group 1: Translating Math Problems")
 st.markdown("""___""")
-st.header("Taking written math problems and using machine translation to compute the answer")
+st.header("Taking written math problems and using transformers to compute the answer")
 st.subheader("Paul Kelly, Carrie Magee, Jack McMorrow, Akshay Verma")
 st.divider()
 
