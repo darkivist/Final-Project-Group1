9eb8656383d3ae39e4371842bd80cef11ad8d30f
darkivist <kpaulj@protonmail.com>
2023-12-10 18:53:12 -0500

    Cleaning up

--- Code/hugging_face_upload.py
+++ Code/hugging_face_upload.py
@@ -7,9 +7,9 @@ from huggingface_hub import Repository, HfFolder, HfApi
 api_token = "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"
 
 # Set the model path
-output_dir = "/home/ubuntu/Code/flan-t5-results/checkpoint-38000/"
+output_dir = "/Users/paulkelly/Desktop/checkpoint-73000"
 tokenizer = T5Tokenizer.from_pretrained(output_dir)
 model = T5ForConditionalGeneration.from_pretrained(output_dir)
 
-model.push_to_hub("darkivist/t5_math_problems")
-tokenizer.push_to_hub("darkivist/t5_math_problems")
+model.push_to_hub("darkivist/t5_base_math_problems")
+tokenizer.push_to_hub("darkivist/t5_base_math_problems")
