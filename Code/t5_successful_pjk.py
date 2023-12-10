25e7852557e45ec5eac5c554ca4ffd8f12657adf
darkivist <kpaulj@protonmail.com>
2023-12-10 13:43:23 -0500

    Removing model visualization.

--- Code/t5_successful_pjk.py
+++ Code/t5_successful_pjk.py
@@ -4,11 +4,10 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration, Seq2SeqTrainer, Seq2SeqTrainingArguments
 from torch.utils.data import Dataset
 import pandas as pd
-import re
 from sklearn.model_selection import train_test_split
 from torch.utils.tensorboard import SummaryWriter
-import torch.onnx
-import netron
+import re
+import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -93,22 +92,21 @@ model = T5ForConditionalGeneration.from_pretrained("t5-small")
 #create custom train/val datasets
 train_dataset = CustomDataset(train_questions, train_equations, train_answers, tokenizer)
 val_dataset = CustomDataset(val_questions, val_equations, val_answers, tokenizer)
-#in the training file we would swap CustomDataset for create_dataloader
 
 #use the dataLoader for training
 model.to(device)
 
 #define training arguments
 training_args = Seq2SeqTrainingArguments(
-    output_dir='./results',
+    output_dir='./results_t5_small',
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
-    logging_dir='./logs',
+    logging_dir='./logs_t5_small',
     logging_steps=100,
     save_steps=1000,
     evaluation_strategy='epoch',
     eval_steps=500,
-    num_train_epochs=1,
+    num_train_epochs=200,
     predict_with_generate=True
 )
 
@@ -123,68 +121,34 @@ trainer = Seq2SeqTrainer(
     optimizers=(optimizer, None)
 )
 
-# Set up TensorBoard writer
+#set up tensorboard writer
 writer = SummaryWriter(log_dir=training_args.logging_dir)
 
-# Training Loop
+#training loop
 for epoch in range(training_args.num_train_epochs):
-    # Training
     trainer.train()
 
-    # Save model checkpoint
+    #save model checkpoint
     if (epoch + 1) % training_args.save_steps == 0:
         model_checkpoint_path = f'./results/checkpoint-{epoch+1}'
         trainer.save_model(model_checkpoint_path)
 
-    # Log histograms of model weights
+    #log histograms of model weights
     for name, param in model.named_parameters():
         writer.add_histogram(name, param, epoch + 1)
 
-    # Check if the current epoch equals the maximum number of epochs
+    #check if the current epoch equals the maximum number of epochs to avoid infinite loop
     if epoch + 1 == training_args.num_train_epochs:
-        break  # Exit the training loop after completing the specified epochs
-
-# Ensure the model is in evaluation mode
-model.eval()
-
-# Dummy input data
-max_length = 128
-dummy_input = {
-    "input_ids": torch.zeros([1, max_length], dtype=torch.long).to(device),
-    "attention_mask": torch.ones([1, max_length], dtype=torch.long).to(device),
-    "equation_ids": torch.ones([1, max_length], dtype=torch.long).to(device),
-    "decoder_input_ids": torch.zeros([1, max_length], dtype=torch.long).to(device)  # Add decoder_input_ids
-}
-
-# Export the model to ONNX format
-torch.onnx.export(
-    model=model,
-    args=(dummy_input,),
-    f='model.onnx',
-    input_names=list(dummy_input.keys()),
-    output_names=['output'],
-    dynamic_axes={
-        'input_ids': {0: 'batch'},
-        'attention_mask': {0: 'batch'},
-        'equation_ids': {0: 'batch'},
-        'decoder_input_ids': {0: 'batch'}  # Add dynamic axis for decoder_input_ids
-    }
-)
-
-# Path to the ONNX model file
-onnx_model_path = 'model.onnx'
-
-# Start netron to visualize the ONNX model
-netron.start(onnx_model_path)
+        break  #exit the training loop after completing the specified epochs
 
 writer.close()
 
-#to launch tensorboard and netron from AWS, enter following in local terminal (update with your own details):
-# "ssh -x -i name_of_your_aws_key.pem 6006:localhost:6006 -L 8080:localhost:8080 ubuntu@ip_address_of_your_instance"
+#to launch tensorboard from AWS, enter following in local terminal (update with your own details):
+# "ssh -x -i name_of_your_aws_key.pem 6006:localhost:6006 ubuntu@ip_address_of_your_instance"
 
 #then in remote terminal enter:
 # "tensorboard --logdir ./logs"
-#then open "http://localhost:6006/" in local web browser for tensorboard and "http://localhost:8080/" for netron graph
+#then open "http://localhost:6006/" in local web browser
 
 #display training metrics
 train_metrics = trainer.evaluate()
@@ -217,7 +181,7 @@ def preprocess_word_problem(problem_text):
     return tokenized_problem
 
 #sample word problem
-word_problem = "Paul has 3.0 books. He gives 1.0 book to Amelia. How many books does Paul have now?"
+word_problem = "Paul has 3.0 books. Amelia has 28.0 books. Paul gives 1.0 book to Amelia. How many books does Paul have now?"
 
 print("Word problem:", word_problem)
 
